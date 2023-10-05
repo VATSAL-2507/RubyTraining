b05b7dd6b150de5109ac466bfbc7ba8e418039cc
VATSAL-2507 <vatsalrprajapati20@gmail.com>
2023-10-05 15:30:10 +0530

    conditional statement and operators

---
+++ bitwise_operator.rb
@@ -0,0 +1,10 @@
+puts "Enter First Value : "
+a = gets.chomp.to_i
+puts "Enter Second Value : "
+b = gets.chomp.to_i
+puts "a & b value is : #{a & b}"
+puts "a | b value is : #{a | b}"
+puts "a ^ b value is : #{a ^ b}"
+puts "a << b value is : #{a << b}"
+puts "a >> b value is : #{a >> b}"
+
